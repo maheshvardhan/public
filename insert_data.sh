702fbe231b42dc1a9d070694a5556f542208c5ee
maheshvardhan <97214719+maheshvardhan@users.noreply.github.com>
2022-05-18 14:41:42 +0530

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE teams, games")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+if [[ $WINNER != "winner" ]]
+then
+TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+if [[ -z $TEAM_ID ]]
+    then
+      INSERT_NAME_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_NAME_RESULT == "INSERT 0 1" ]]
+      then
+      echo Inserted into teams, $WINNER
+      fi
+      TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+      fi
+fi
+if [[ $OPPONENT != "opponent" ]]
+then
+TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+if [[ -z $TEAM_ID ]]
+    then
+      INSERT_NAME_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_NAME_RESULT == "INSERT 0 1" ]]
+      then
+      echo Inserted into teams, $OPPONENT
+      fi
+      TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+      fi
+fi
+
+WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+if [[ -n $WINNER || -n $OPPONENT ]]
+then
+if [[ $YEAR != "year" ]]
+    then
+      INSERT_GAMES_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+      if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+      then
+      echo Inserted into games, $YEAR : $ROUND : $WINNER_ID : $OPPONENT_ID : $WINNER_GOALS : $OPPONENT_GOALS
+      fi 
+    fi 
+fi
+
+done
